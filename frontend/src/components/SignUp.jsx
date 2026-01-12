ed7c977f8ed8aafcea11c7246a454f25ff7ec83b
yannuser <hienpollo@gmail.com>
2026-01-11 19:29:38 -0500

    sign up frontend

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,11 +1,33 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import axios from 'axios';
 
 export default function SignUp() {
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("");
+    const [dateOfBirth, setDateOfBirth] = useState(new Date());
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
 
     async function handleSubmit(e) {
         e.preventDefault();
+
+        axios.post("http://localhost:5000/User/register", {firstname, lastname, dateOfBirth, email, password}).then(
+            result => {
+                console.log(result);
+                if (result.status == 201) {
+                    navigate('/');
+                } else {
+                    alert("An error occurred!!!")
+                }
+                
+            }
+        ).catch(err => console.log(err))
     }
+    
 
     return(
         <>
@@ -13,23 +35,23 @@ export default function SignUp() {
             <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="firstname">First name</label>
-                    <input type="text" id="firstname" />
+                    <input type="text" id="firstname"  value={firstname} onChange={e => setFirstname(e.target.value)} required/>
                 </div>
                 <div>
                     <label htmlFor="lastname">Last name</label>
-                    <input type="text" id="lastname" />
+                    <input type="text" id="lastname" value={lastname} onChange={e => setLastname(e.target.value)} required/>
                 </div>
                 <div>
                     <label htmlFor="dateOfBirth">Date Of Birth ( not required )</label>
-                    <input type="date" id="dateOfBirth" />
+                    <input type="date" id="dateOfBirth" value={dateOfBirth} onChange={e => setDateOfBirth(e.target.value)}/>
                 </div>
                 <div>
                     <label htmlFor="email">Email</label>
-                    <input type="email" id="email"/>
+                    <input type="email" id="email" value={email} onChange={e => setEmail(e.target.value)} required/>
                 </div>
                 <div>
                     <label htmlFor="password">Password</label>
-                    <input type="password" />
+                    <input type="password" id="password" value={password} onChange={e => setPassword(e.target.value)} required/>
                 </div>
                 <input type="submit"/>
             </form>
